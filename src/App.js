8f4c8769821fd7dbd9a8d7e7c553d50cc88c93fd
Hemant Verma <vermahemant837@gmail.com>
2023-10-22 12:18:17 +0530

    Mail Box optimisations

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { authActions } from "./store/auth-slice";
 import router from "./components/Routes/Routes";
 import { userDataActions } from "./store/userData-slice";
-
-// let isInitial = true;
+import useDataFetching from "./components/Hooks/dataFetching";
 
 function App() {
   const baseURL = "https://mail-box-client-8c444-default-rtdb.firebaseio.com/";
@@ -36,79 +35,21 @@ function App() {
       };
       fetchUserLogin();
     }
-    // Function to fetch the list of emails
-    const fetchEmailList = async () => {
-      try {
-        const response = await axios.get(
-          `${baseURL}/inbox/${formatEmail(currentUserEmail)}.json`
-        );
-        const newInboxDatas = Object.keys(response.data).map((key) => {
-          return { firebaseId: key, ...response.data[key] };
-        });
-
-        dispatch(userDataActions.replaceInboxData(newInboxDatas));
-
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    // call to fetch emails
-    fetchEmailList();
-
-    // Use setInterval to call the API every 2 seconds
-    const intervalId = setInterval(fetchEmailList, 2000);
-
-    // Cleanup the interval when the component unmounts
-    return () => clearInterval(intervalId);
   }, [currentUserEmail, dispatch]);
 
-  // console.log(currentUserEmail);
+// Use the custom hook for inbox data
+useDataFetching(
+  `${baseURL}/inbox/${formatEmail(currentUserEmail)}.json`,
+  userDataActions.replaceInboxData,
+  dispatch
+);
 
-  // for fetching sentDatas and inboxDatas
-  useEffect(() => {
-    const fetchAllDatas = async () => {
-      try {
-        const fetchSentData = await axios.get(
-          `${baseURL}/sent/${formatEmail(currentUserEmail)}.json`
-        );
-
-        // console.log("fetchSentData", Object.values(fetchSentData.data));
-
-        const newSentDatas = Object.keys(fetchSentData.data).map((key) => {
-          return { firebaseId: key, ...fetchSentData.data[key] };
-        });
-
-        // console.log("newSentDatas", newSentDatas);
-
-        dispatch(userDataActions.replaceSentData(newSentDatas));
-
-        dispatch(userDataActions.setSentEmail(newSentDatas[0].to));
-
-        // const fetchInboxData = await axios.get(
-        //   `${baseURL}/inbox/${formatEmail(currentUserEmail)}.json`
-        // );
-
-        // const newInboxDatas = Object.keys(fetchInboxData.data).map((key) => {
-        //   return { firebaseId: key, ...fetchInboxData.data[key] };
-        // });
-
-        // // console.log("newInboxDatas", newInboxDatas);
-
-        // dispatch(userDataActions.replaceInboxData(newInboxDatas));
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    fetchAllDatas();
-  }, [currentUserEmail, dispatch]);
-
-  // if (sentEmail) {
-  //   console.log("sentEmail", sentEmail);
-  // }
-
-  // for fetching inboxDatas
+ // Use the custom hook for sent data
+ useDataFetching(
+  `${baseURL}/sent/${formatEmail(currentUserEmail)}.json`,
+  userDataActions.replaceSentData,
+  dispatch
+);
 
   return <RouterProvider router={router}></RouterProvider>;
 }
---
+++ src/components/Hooks/dataFetching.js
@@ -0,0 +1,32 @@
+// (custom hook)
+
+import { useEffect } from "react";
+import axios from "axios";
+
+function useDataFetching(url, dataKey, dispatch) {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(url);
+        const data = Object.keys(response.data).map((key) => ({
+          firebaseId: key,
+          ...response.data[key],
+        }));
+        dispatch(dataKey(data));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    // Initial fetch
+    fetchData();
+
+    // Set up an interval to fetch data every 2 seconds
+    const intervalId = setInterval(fetchData, 2000);
+
+    // Clean up the interval on unmount
+    return () => clearInterval(intervalId);
+  }, [url, dispatch, dataKey]);
+}
+
+export default useDataFetching;
